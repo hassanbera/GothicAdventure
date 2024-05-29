878608adc686b9b3d454680714b5a0812e277bd9
Hasan <128808077+hasanneke@users.noreply.github.com>
2024-05-29 18:57:58 +0300

    updated

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Fruit : MonoBehaviour
 {
-    public int attackBonus = 12; // Amount of health to add
+    public int attackBonus = 12; // Amount of attack bonus to add
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,21 +12,21 @@ public class Fruit : MonoBehaviour
             IPlayer player = other.GetComponent<IPlayer>();
             if (player != null)
             {
-                IPlayer tempPlayer = player;
-                // Apply health boost decorator
-                tempPlayer = new AttackBoost(player, attackBonus);
-                tempPlayer.ApplyBoost();
-                StartCoroutine(RemoveBoost(attackBonus));
-
-                player = new AttackBoost(player, -attackBonus);
-                player.ApplyBoost();
+                // Apply attack boost decorator
+                IPlayer boostedPlayer = new AttackBoost(player, attackBonus);
+                boostedPlayer.ApplyBoost();
+                StartCoroutine(RemoveBoost(boostedPlayer, attackBonus));
             }
             Destroy(gameObject); // Destroy the fruit after collision
         }
     }
 
-    public IEnumerator RemoveBoost(int attackBonus)
+    private IEnumerator RemoveBoost(IPlayer player, int attackBonus)
     {
         yield return new WaitForSeconds(5);
+
+        // Apply negative attack boost to remove the previous bonus
+        IPlayer boostedPlayer = new AttackBoost(player, -attackBonus);
+        boostedPlayer.ApplyBoost();
     }
 }
