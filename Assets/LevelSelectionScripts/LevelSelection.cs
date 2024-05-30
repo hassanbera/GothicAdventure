1644b0a96a92ba80d12870df81dec0548e3c55a2
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2024-05-30 06:23:31 +0300

    Level Selection added

---
+++ Assets/LevelSelectionScripts/LevelSelection.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
+
